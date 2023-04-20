fb975a65017fa79a7e83445a7a69bb4287681c60
wujunpeng <wujunpeng@wps.cn>
2023-04-20 10:57:03 +0800

    feat: 补充对环境变量的读取的示例

--- junior/get-struct-size/main.go
+++ junior/get-struct-size/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"unsafe"
 )
 
@@ -25,4 +26,9 @@ func main() {
 	fmt.Println(m2.GetLength()) // 88
 	m3 := MyStruct{Name: "小明"}
 	fmt.Println(m3.GetLength()) // 88
+
+	// 实际上，如果要读取系统的KEYA变量，Goland的run，是需要去 配置->Go modules->自己新增自定义key-val的，因为Goland默认就是读取他自己的go环境变量列表
+	// 如果使用 命令行直接跑 go run main.go 读取的则是我们本机系统的关键变量，此时，使用 ~/.zshrc 中 'export KEYA=123213123123aaaaa'
+	keyA := os.Getenv("KEYA")
+	fmt.Println(keyA)
 }
