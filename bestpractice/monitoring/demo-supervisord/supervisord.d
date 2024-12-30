2433392047a102e1c163d96951009cc5685f0f9d
jasper <1049578026@qq.com>
2024-12-30 15:34:01 +0800

    chore: 删除部分目录

--- bestpractice/monitoring/demo-supervisord/supervisord.d
+++
@@ -1,8 +0,0 @@
-[program:demo-supervisord]
-directory=/Users/jasper/projects/GolandProjects/go-be-professor/bestpractice/monitoring/demo-supervisord
-command=go run demo-supervisord 2>/Users/jasper/projects/GolandProjects/go-be-professor/bestpractice/monitoring/demo-supervisord/gc.log
-stdout_logfile=/Users/jasper/projects/GolandProjects/go-be-professor/bestpractice/monitoring/demo-supervisord/stdout.log
-stdout_logfile_backups=50
-redirect_stderr=true
-autostart=true
-autorestart=true
